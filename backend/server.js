65e7d17e902f0c576812c01ec57a2043611aa8e0
pilauria <ptr.lauria@gmail.com>
2021-12-06 14:28:03 +0100

    sample data

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import colors from 'colors';
 import products from './data/products.js';
 
 dotenv.config();
@@ -29,5 +30,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
+      .bold
+  )
 );
