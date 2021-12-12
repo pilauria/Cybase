cb217488a2647f4c1b7f4cca549acbd910f63be7
pilauria <ptr.lauria@gmail.com>
2021-12-12 20:07:28 +0100

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
@@ -7,6 +8,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -26,10 +28,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// make the 'uploads' folder STATIC, so that it can be loaded in the browser
+const __dirname = path.resolve(); // to bring dirname in es6 module otherwise we can't use it with es6 modules
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 
